7abfc79e408efb331bb823c8fe2c191bbf8c53b4
jonp001 <jonperez01@outlook.com>
2023-06-13 18:42:05 -0400

    added favoriting/unfavoriting bicycle listings, had to revert code due to an error i made

--- routes/bicycle.routes.js
+++ routes/bicycle.routes.js
@@ -5,14 +5,11 @@ const User= require("../models/User.model");
 const isLoggedIn = require("../utils/isLoggedIn");
 const uploader = require("../config/cloudinary");
 
-
 router.get("/bicycles/new", isLoggedIn, (req, res, next) => {
     res.render("bicycles/new-bicycle");
 });
 
-
 router.post ("/bicycles/create", isLoggedIn, uploader.single("img"), (req, res, next) => {
-
     Bicycle.create({
         img: req.file.path,
         title: req.body.bicycleTitle,
@@ -32,20 +29,55 @@ router.get("/bicycles", (req, res, next) => {
     console.log(req.session);
     Bicycle.find()
     .then((allTheBicycles) => {
-        res.render("bicycles/bicycle-list", {Bicycle: allTheBicycles})
+        res.render("bicycles/bicycle-list", {bicycles: allTheBicycles})
     })
     .catch((error) => next(error));
-})
-
-
-router.get("/bicycles/:id", (req, res, next) => {
-    const theID= req. params.id;
-    Bicycle.findById(theID)
-   .then((theBicycle) => {
-    res.render("bicycles/bicycle-details", {bicycle: theBicycle})
-   })
-   .catch((error) => next(error));
 });
 
+router.post("/bicycles/:id/favorited", isLoggedIn, (req, res, next) => {
+    User.findByIdAndUpdate(
+        req.session.currentUser._id,
+        { $addToSet: { favoriteBicycle: req.params.id} },
+        { new: true }
+    )
+    .populate("favoriteBicycle")
+    .then(user => {
+        req.flash("success", "Bicycle added to favorites list");
+        res.redirect("/bicycles");
+     })
+     .catch((error) => next(error));
+});
 
-module.exports= router
+router.get("/bicycles/favorited", isLoggedIn, (req, res, next) => {
+    User.findById(req.session.currentUser._id)
+    .populate("favoriteBicycle")
+    .then(user => {
+        res.render("bicycles/bicycle-list", {bicycles: user.favoriteBicycle})
+    })
+    .catch(error => next(error));
+});
+
+router.post("/bicycles/:id/unfavorite", isLoggedIn, (req, res, next) => {
+    User.findByIdAndUpdate(
+        req.session.currentUser._id,
+        {$pull : {favoriteBicycle: req.params.id} },
+        {new: true}
+    )
+    .populate("favoriteBicycle")
+    .then(user => {
+        req.flash("success", "Bicycle removed from favorites list");
+        res.redirect("/bicycles");
+    })
+    .catch(error => next(error));
+});
+
+router.get("/bicycles/:id", (req, res, next) => {
+    const theID= req.params.id;
+    Bicycle.findById(theID)
+    .then((theBicycle) => {
+        res.render("bicycles/bicycle-details", {bicycle: theBicycle})
+    })
+    .catch((error) => next (error))
+});
+
+module.exports = router;
